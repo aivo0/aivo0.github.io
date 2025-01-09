938948496982c073d9fa8e9d5885429ae807b6d0
Aivo Olev <aivo.olev@cyber.ee>
2025-01-09 16:33:37 +0200

    Basic express app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Without middleware
+app.get('/', function (req, res) {
+    res.redirect('/user');
+});
+
+app.get('/user', function (req, res) {
+    res.send("Redirected to User Page");
+});
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
